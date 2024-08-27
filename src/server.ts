c4815f1b484a395bb083ccaa52a8c6ae0166e3bb
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-27 18:15:12 -0300

    chore: Update server configuration to include body-parser middleware for handling larger JSON payloads

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './config/db'
 import measureRoutes from './routes/measureRoutes'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 connectDB()
@@ -17,7 +18,9 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 )
-app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+
 app.use('/api/measures', measureRoutes)
 
 app.listen(PORT, () => {
