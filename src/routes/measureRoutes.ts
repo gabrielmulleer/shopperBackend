be980d3c15d5a4e92900a32372af84d736b51c1d
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-28 09:54:57 -0300

    feat: Add get route to retrieving measures by customer code

--- src/routes/measureRoutes.ts
+++ src/routes/measureRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
-import { confirmMeasure, uploadMeasure } from '../controllers/measureController'
+import {
+  confirmMeasure,
+  listMeasuresByCustomer,
+  uploadMeasure,
+} from '../controllers/measureController'
 
 const router = Router()
 
 router.post('/upload', uploadMeasure)
 router.patch('/confirm', confirmMeasure)
+router.get('/:customerCode/list', listMeasuresByCustomer)
 
 export default router
