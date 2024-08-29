85434751d7738ffa0e2000bdd92811303d4a50d7
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-29 12:46:15 -0300

    refactor: update uploadMeasure controller to handle base64 image data

--- src/controllers/measureController.ts
+++ src/controllers/measureController.ts
@@ -4,6 +4,7 @@ import { processImageWithGeminiAPI } from '../services/geminiService'
 import moment from 'moment'
 import { generateTempUrl, saveBase64Image } from '../utils/generate-image'
 import { v4 as uuidv4 } from 'uuid'
+import { extractBase64FromDataUri } from '../utils/extract-base64'
 
 // Função para validar se a string é base64
 const isBase64 = (str: string) => {
@@ -17,7 +18,7 @@ const isBase64 = (str: string) => {
 export const uploadMeasure = async (req: Request, res: Response) => {
   try {
     const { customer_code, image, measure_datetime, measure_type } = req.body
-
+    const base64Image = extractBase64FromDataUri(image)
     // Validações
     if (!customer_code || typeof customer_code !== 'string') {
       return res.status(400).json({
@@ -26,7 +27,11 @@ export const uploadMeasure = async (req: Request, res: Response) => {
       })
     }
 
-    if (!image || typeof image !== 'string' || !isBase64(image)) {
+    if (
+      !base64Image ||
+      typeof base64Image !== 'string' ||
+      !isBase64(base64Image)
+    ) {
       return res.status(400).json({
         error_code: 'INVALID_DATA',
         error_description:
@@ -68,10 +73,13 @@ export const uploadMeasure = async (req: Request, res: Response) => {
     }
 
     // Chama o serviço para processar a imagem usando a API do Google Gemini
-    const geminiResult = await processImageWithGeminiAPI(image, measure_type)
+    const geminiResult = await processImageWithGeminiAPI(
+      base64Image,
+      measure_type,
+    )
     const fileName = `${geminiResult.uuid || uuidv4()}.jpg`
 
-    await saveBase64Image(fileName, image)
+    await saveBase64Image(fileName, base64Image)
     // Gere uma URL temporária para a imagem
     const imageUrl = generateTempUrl(req, fileName)
 
